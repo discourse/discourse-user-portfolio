b4c52fbedeea08e8d966d321f8095a5ae9920397
David Taylor <david@taylorhq.com>
2020-06-22 21:26:00 +0100

    FIX: Allow using gallery view for portfolio on mobile devices
    This is a temporary workaround, pending a permanent fix in https://github.com/discourse/discourse/pull/10105

---
+++ javascripts/discourse/components/portfolio-basic-topic-list.js
@@ -0,0 +1,4 @@
+import BasicTopicList from "discourse/components/basic-topic-list";
+// Hack to work around https://github.com/discourse/discourse/pull/10105
+// Can be removed once the PR is merged to core
+export default BasicTopicList;
