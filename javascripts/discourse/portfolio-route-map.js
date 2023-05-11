2e6d91ff2f01bfc311deaf7fc061ac84912796c1
awesomerobot <kris.aubuchon@discourse.org>
2023-05-11 18:14:50 -0400

    UX: fix link for user menu, add requested settings

--- javascripts/discourse/portfolio-route-map.js
+++ javascripts/discourse/portfolio-route-map.js
@@ -1,6 +1,6 @@
 export default {
   resource: "user",
   map() {
-    this.route("portfolio", { path: "activity/portfolio" });
+    this.route("portfolio", { path: `activity/${settings.portfolio_route_name}` });
   },
 };
